34fa223576973d65fffe0df7aafc28839c767443
tama <tama@ttk1.net>
2024-05-02 02:56:46 +0900

    sys.implementation.name を見て import する方を切り替えるようにした

--- bme680/__init__.py
+++ bme680/__init__.py
@@ -1,1 +1,6 @@
-from bme680.bme680 import BME680
+import sys
+
+if sys.implementation.name == "micropython":
+    from bme680.bme680_micropython import BME680
+else:
+    from bme680.bme680 import BME680
